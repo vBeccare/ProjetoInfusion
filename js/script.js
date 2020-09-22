7f196d01d6eec69508b41159ea644324b4c67f08
Victor Beccare <vbeccare@redventures.com>
2020-09-22 17:37:25 -0300

    Terminando css e adicionando JS

---
+++ js/script.js
@@ -0,0 +1,24 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js');
+    $body.classList.add('js');
+
+
+    var $btnMenu = document.querySelector('.header__btnMenu');
+    
+
+    
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1024
+    })
+
+    var carousel = new Carousel ({
+        container: '.slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+})()
